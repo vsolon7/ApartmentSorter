f19e44f0f268440fa78d7e68c26eb75812b1a96e
Ves <vsolon7@gmail.com>
2018-01-15 11:57:20 -0600

    The scraper now gets the actual square footage of the apartment. Might make it include extra data sometime in the future but for now this is good.

--- scraper.py
+++ scraper.py
@@ -15,52 +15,78 @@ with open('ApartmentData/URLsToScrape.csv') as csvfile:
         for column in row:
             URLsToScrape.append(column)
 
-#open the page(s) and add the HTML to pages to scrape
+#open the page(s) and add the HTML to pages to scrape. then make it a BS object
+c = 0
 for URL in URLsToScrape:
+    c += 1
     client = urllib.request.urlopen(URL)
     rawSource = client.read()
-    pagesToScrape.append(rawSource)
+    bsPages.append(bs.BeautifulSoup(rawSource, 'lxml'))
     client.close()
-
-#make the HTML into a BS object
-for x in range(0, len(pagesToScrape)):
-   bsPages.append(bs.BeautifulSoup(pagesToScrape[x], 'lxml'))
+    print('Loaded URL #', c, '!', sep='')
 
 #so in this, the outer loop goes through all the beautiful soup pages. the inner loop looks through everything
 #and stores it.
+c = 0
 for page in bsPages:
     #this is a regular expression. it finds everything that begins with "level". their site was weird and they all started with different things
     allListings = page.find_all("div", {"class":re.compile("^level")})
 
     #goes through every listing in the list of all apartment listings. lol.
     for listing in allListings:
+        c += 1
         #this will store the data for a single apartment. will later be appended to a list of lists that has all apartments
         tempDataSet = []
         #regular expressions are so good
-        propInfo = listing.find_all("div", {"class":re.compile("^propertyInfo xs-")})
+        propInfo = listing.find_all("div", {"class":re.compile("^propertyInfo xs-")})[0]
 
         #get the rent. i opted to just grab the cheapest one, also i removed commas
-        tempR = propInfo[0].find_all("span", {"class": "altRentDisplay"})[0].text.strip().replace(',', '')
-        tempR = tempR.split(' -', 1)[0]
-        tempDataSet.append(tempR)
+        rent = propInfo.find_all("span", {"class": "altRentDisplay"})[0].text.strip().replace(',', '')
+        rent = rent.split(' -', 1)[0]
+        rent = rent.split('$',1)[1]
+
+        # get the city
+        city = propInfo.find_all("span", {"itemprop": "addressLocality"})[0].text.strip()
 
         #get the address, i removed the commas to help put it in a .csv
-        tempDataSet.append(propInfo[0].find_all("span", {"itemprop":"address"})[0].text.strip().replace(',', ''))
+        address = propInfo.find_all("span", {"itemprop":"address"})[0].text.strip().replace(',', '')
 
         #get the name of the post
-        for name in propInfo[0].find_all('a'):
-            tempDataSet.append(name.get('title'))
+        name = ''
+        for na in propInfo.find_all('a'):
+           name = na.get('title')
 
-        #get the page link
-        for link in propInfo[0].find_all('a'):
-            tempDataSet.append(link.get('href'))
+        #get the link to the actual main apartment page
+        link = ''
+        for li in propInfo.find_all('a'):
+            link = li.get('href')
 
+        #now we open the link to get the square footage and possible extra data.
+        client = urllib.request.urlopen(link)
+        subPage = client.read()
+        client.close()
+        SPBS = bs.BeautifulSoup(subPage, 'lxml')
+
+        #narrow the search a bit because when I did it before, I got square footage from tons of other apartment sizes
+        narrowedSearch = SPBS.find("div", {"data-beds":"2"})
+        squareFeet = narrowedSearch.find("div", "xs-12 md-4 sqft").text.strip().replace(',','')
+        squareFeet = squareFeet.split(' ',1)[0]
+
+        tempDataSet.append(rent)
+        tempDataSet.append(squareFeet)
+        tempDataSet.append(city)
+        tempDataSet.append(address)
+        tempDataSet.append(link)
+        tempDataSet.append(name)
         #add it to the list of apartments found
         apartmentScrapeList.append(tempDataSet)
 
-#i know theres better ways to write to a .csv, but eh
+        #a little info to tell us when we're done with each apartment
+        print('Apartment #', c, ' done!', sep='')
+
+#i know there's better ways to write to a .csv, but eh
 scrapeOutput = open('ApartmentData/scrapedData.csv', 'w')
-string = 'PRICE,ADDRESS,NAME,URL'
+string = 'RENT,SQUARE FEET,CITY,ADDRESS,URL,NAME'
 for x in apartmentScrapeList:
     for y in x:
         string += (y + ', ')
