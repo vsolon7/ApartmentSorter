b188b0d97f234f19b363fceb484006308dc43ea7
Ves <vsolon7@gmail.com>
2018-01-15 12:27:25 -0600

    No time to change comments, gotta leave but it now sorts it by price per square foot and I successfully sorted 500 apartments in my area!

--- apartment.py
+++ apartment.py
@@ -4,32 +4,14 @@ class Apartment:
     sizeWeight = .45
     ratingWeight = 0
 
-    #these are the starting values out of all the apartments. for normalization
-    minP = 0
-    maxP = 0
-    minS = 0
-    maxS = 0
-    minR = 0
-    maxR = 0
-
-    def __init__(self, p, n, s, r):
+    def __init__(self, p, s, c, a, u, n):
         self.price = p
-        self.name = n
         self.size = s
-        self.rating = r
+        self.city = c
+        self.address = a
+        self.url = u
+        self.name = n
+
 
     def calculateWeight(self):
-        self.nPrice = (self.price - self.minP) / (self.maxP - self.minP)
-        self.nSize = (self.size - self.minS) / (self.maxS - self.minS)
-        self.nRating = (self.rating - self.minR) / (self.maxR - self.minR)
-
-        self.weightedNum = ((1-self.nPrice) * self.priceWeight) + (self.nSize * self.sizeWeight) + (
-        self.nRating * self.ratingWeight)
-
-    def setMinMax(self, miP, maP, miS, maS, miR, maR):
-        self.minP = miP
-        self.maxP = maP
-        self.minS = miS
-        self.maxS = maS
-        self.minR = miR
-        self.maxR = maR
+        self.weightedNum = self.price / self.size
--- sorter.py
+++ sorter.py
@@ -8,34 +8,29 @@ ApartmentList = []
 
 #reads the .csv data, I hand copied this from a website
 #Later I might web scrape it? I'm not sure
-with open('ApartmentData/apartments.csv') as csvFile:
+with open('ApartmentData/scrapedData.csv') as csvFile:
     readCSV = csv.reader(csvFile, delimiter = ',')
 
     for row in readCSV:
-        ApartmentList.append(Apartment(int(row[0]), row[1], int(row[2]), int(row[3])))
+        price = int(row[0])
+        squareFt = row[1]
+        city = row[2]
+        address = row[3]
+        url = row[4]
+        name = row[5]
+        newSquareFt = 0
 
+        if squareFt == ' ':
+            newSquareFt = 1
+        else:
+            newSquareFt = int(squareFt)
 
+        ApartmentList.append(Apartment(price, newSquareFt, city, address, url, name))
 
-#calculuate min and max for normalization
-#very long and if statement-y. basically finds the ranges of the apartments
-newMinP = 100000
-newMinS = 100000
-newMinR = 100000
-newMaxP = 0
-newMaxS = 0
-newMaxR = 0
-for ap in ApartmentList:
-    newMinP = ap.price if (ap.price < newMinP) else newMinP
-    newMaxP = ap.price if (ap.price > newMaxP) else newMaxP
-    newMinS = ap.size if (ap.size < newMinS) else newMinS
-    newMaxS = ap.size if (ap.size > newMaxS) else newMaxS
-    newMinR = ap.rating if (ap.rating < newMinR) else newMinR
-    newMaxR = ap.rating if (ap.rating > newMaxR) else newMaxR
 
 
 #sets the mins and maxes we just found and calculates the weighted number to sort the apartments
 for ap in ApartmentList:
-    ap.setMinMax(newMinP, newMaxP, newMinS, newMaxS, newMinR, newMaxR)
     ap.calculateWeight()
 
 
@@ -44,7 +39,7 @@ for ap in ApartmentList:
 for x in range(1, len(ApartmentList)):
     ap2 = ApartmentList[x]
     y = x - 1
-    while (y >= 0) and (ApartmentList[y].weightedNum < ap2.weightedNum):
+    while (y >= 0) and (ApartmentList[y].weightedNum > ap2.weightedNum):
         ApartmentList[y + 1] = ApartmentList[y]
         y = y - 1
     ApartmentList[y + 1] = ap2
@@ -62,7 +57,7 @@ for ap in ApartmentList:
 outputSorted = open('ApartmentData/sortedApartments.csv', 'w')
 
 for ap in ApartmentList:
-    string = str(ap.price) + ',' + str(ap.name) + ',' + str(ap.size) + ',' + str(ap.rating) + ',' + str(ap.weightedNum) + '\n'
+    string = str(ap.price) + ',' + str(ap.size) + ',' + str(ap.city) + ',' + str(ap.address) + ',' + ap.url + ',' + str(ap.weightedNum) + '\n'
     outputSorted.write(string)
 
 outputSorted.close()
